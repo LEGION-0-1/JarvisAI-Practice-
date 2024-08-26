7410f381b72feb7ec0472d516e38da665b39d199
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-26 13:20:50 +0530

    searching possible

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ def speak(s):
     speaker = win32com.client.Dispatch("SAPI.SpVoice")
     speaker.Speak(s)
 
+def conditions(s):
+    site = ["Youtube", "Github"]
+    link = ["https://www.youtube.com", "https://github.com/LEGION-0-1"]
+    if(f'Open {site}' or f'Search {site}'):
+        if (s == f'Open {site[0]}'.lower()):
+            speak(f"Opening {site[0]}")
+            webbrowser.open(f"{link[0]}")
+        elif (s == f'Open {site[1]}'.lower()):
+            speak(f"Opening {site[1]}")
+            webbrowser.open(f"{link[1]}")
+        elif (s == 'Search'.lower()):
+            print("What should I search?")
+            a = input()
+            webbrowser.open(f"https://www.google.com/search?q={a}")
+    else:
+        speak(s)
+
 while 1:
     print("What do you want me to speak")
     s = input()
-    site = ["Youtube","Github"]
-    link = ["https://www.youtube.com","https://github.com/LEGION-0-1"]
-    if(s == f'Open {site[0]}'.lower()):
-        speak(f"Opening {site[0]}")
-        webbrowser.open(f"{link[0]}")
-    elif(s == f'Open {site[1]}'.lower()):
-        speak(f"Opening {site[1]}")
-        webbrowser.open(f"{link[1]}")
+    conditions(s)
