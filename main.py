b7fa938a6ea8af49a41d4dbc1fdb85da1e22c9fe
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-26 12:20:30 +0530

    if else statement to open websites

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import win32com.client
+import webbrowser
 
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
+def speak(s):
+    speaker = win32com.client.Dispatch("SAPI.SpVoice")
+    speaker.Speak(s)
 
 while 1:
     print("What do you want me to speak")
     s = input()
-    speaker.Speak(s)
+    site = ["Youtube","Github"]
+    link = ["https://www.youtube.com","https://github.com/LEGION-0-1"]
+    if(s == f'Open {site[0]}'.lower()):
+        speak(f"Opening {site[0]}")
+        webbrowser.open(f"{link[0]}")
+    elif(s == f'Open {site[1]}'.lower()):
+        speak(f"Opening {site[1]}")
+        webbrowser.open(f"{link[1]}")
